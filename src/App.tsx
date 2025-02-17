13a5fc3121290036b16aa06d284c391913b011d9
sulenq <fatwalinovera@gmail.com>
2025-02-17 16:56:38 +0700

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,18 @@ import "./App.css";
 import { Toaster } from "./components/ui/toaster";
 import Routing from "./routes/Routing";
 import theme from "./theme";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [reload, setReload] = useState(false);
+  useEffect(() => {
+    const themeMode = localStorage.getItem("theme");
+    if (!themeMode || themeMode === "system") {
+      localStorage.setItem("theme", "light");
+      setReload(!reload);
+    }
+  }, []);
+
   return (
     <ChakraProvider value={theme}>
       <Toaster />
